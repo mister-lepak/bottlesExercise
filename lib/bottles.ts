06d3e640cd766324d72552bef88d5c7ea70e06ea
Try Wijono <3wijono@gmail.com>
2021-04-25 18:02:44 +0800

    added typescript

--- lib/bottles.ts
+++ lib/bottles.ts
@@ -1,28 +1,29 @@
 // import { downTo } from "./helpers";
 
 class Bottles {
-  verse(num) {
-    let bottleWordBefore;
-    let bottleWordAfter;
-    let numAfter = num - 1;
-    let itOrOne = "one";
-    let numWord1;
-    let numWord2;
 
-    num !== 1 ? (bottleWordBefore = "bottles") : (bottleWordBefore = "bottle");
+  verse(num) {
+    let bottleWordBefore: string;
+    let bottleWordAfter: string
+    let numAfter: number | string = num - 1;
+    let itOrOne: string = 'one';
+    let numWord1: string;
+    let numWord2: string;
+
+    num !== 1 ? (bottleWordBefore = 'bottles') : (bottleWordBefore = 'bottle');
     num - 1 !== 1
-      ? (bottleWordAfter = "bottles")
-      : (bottleWordAfter = "bottle");
+      ? (bottleWordAfter = 'bottles')
+      : (bottleWordAfter = 'bottle');
     if (num - 1 === 0) {
-      numAfter = "no more";
-      itOrOne = "it";
+      numAfter = 'no more';
+      itOrOne = 'it';
     }
     let buyOrNo = `Take ${itOrOne} down and pass it around, `;
     if (num === 0) {
-      buyOrNo = "Go to the store and buy some more, ";
+      buyOrNo = 'Go to the store and buy some more, ';
       num = undefined;
-      numWord1 = "No more";
-      numWord2 = "no more";
+      numWord1 = 'No more';
+      numWord2 = 'no more';
       numAfter = 99;
     }
 
@@ -34,9 +35,9 @@ class Bottles {
   }
 
   verses(startNum, endNum) {
-    let iteratedVerses = "";
+    let iteratedVerses: string = '';
     for (let i = startNum; i >= endNum; i--) {
-      if (i !== startNum) iteratedVerses = iteratedVerses.concat("\n");
+      if (i !== startNum) iteratedVerses = iteratedVerses.concat('\n');
       iteratedVerses = iteratedVerses.concat(this.verse(i));
     }
 
